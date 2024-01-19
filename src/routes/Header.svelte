97f7889854cb18a3958677410c38652ddd4f4f40
Christopher Ødegård <christopher.odegard@kongsbergdigital.com>
2024-01-19 12:57:53 +0100

    added base to links

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,24 +2,25 @@
 	import { page } from '$app/stores';
 	import { count } from './todo/+page.svelte';
 	import logo from '$lib/images/logo.png';
+	import { base } from "$app/paths";
 </script>
 
 <header class="navbar">
 	<section class="navbar-section">
 		<a
-			href="/todo"
+			href="{base}/todo"
 			class="btn btn-link"
 			aria-current={$page.url.pathname.startsWith('/todo') ? 'page' : undefined}
 		>
 			<span class="badge" data-badge="{$count}">Todo</span>
 		</a>
 		<a
-			href="/about"
+			href="{base}/about"
 			class="btn btn-link"
 			aria-current={$page.url.pathname.startsWith('/about') ? 'page' : undefined}>About</a
 		>
 		<a
-			href="/sverdle"
+			href="{base}/sverdle"
 			class="btn btn-link"
 			aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>Game</a
 		>		
