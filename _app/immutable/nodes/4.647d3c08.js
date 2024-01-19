226bd3d69cab3759a73f1f86ce89f7640dacebb7
Christopher Ødegård <christopher.odegard@kongsbergdigital.com>
2024-01-19 13:21:59 +0100

    Updates

---
+++ _app/immutable/nodes/4.647d3c08.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.6775f91d.js";import{S as t,i as e}from"../chunks/index.15516d7b.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
