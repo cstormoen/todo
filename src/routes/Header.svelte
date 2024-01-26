4433dcb10b2fa9aaebc0ddeeb057d59d4ccd2a8e
Christopher Ødegård <christopher.odegard@kongsbergdigital.com>
2024-01-26 15:15:11 +0100

    added a simple media query

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,35 +2,47 @@
 	import { page } from '$app/stores';
 	import { count } from './todo/+page.svelte';
 	import logo from '$lib/images/logo.png';
-	import { base } from "$app/paths";
+	import { base } from '$app/paths';
 </script>
 
 <header class="navbar">
 	<section class="navbar-section">
-		<a
-			href="{base}/todo"
-			class="btn btn-link"
-			aria-current={$page.url.pathname.startsWith('/todo') ? 'page' : undefined}
-		>
-			<span class="badge" data-badge="{$count}">Todo</span>
-		</a>
-		<a
-			href="{base}/about"
-			class="btn btn-link"
-			aria-current={$page.url.pathname.startsWith('/about') ? 'page' : undefined}>About</a
-		>
+		<div class="navbar-menu" id="nav-items">
+			<a
+				href="{base}/todo"
+				class="btn btn-link"
+				aria-current={$page.url.pathname.startsWith('/todo') ? 'page' : undefined}
+			>
+				<span class="badge" data-badge={$count}>Todo</span>
+			</a>
+			<a
+				href="{base}/about"
+				class="btn btn-link"
+				aria-current={$page.url.pathname.startsWith('/about') ? 'page' : undefined}>About</a
+			>
+		</div>
 	</section>
 	<section class="navbar-center">
 		<figure class="s-circle">
-			<img src="{logo}" alt="Logo" />
+			<img src={logo} alt="Logo" />
 		</figure>
 	</section>
-	<section class="navbar-section">
+	<section id="created-by" class="navbar-section">
 		<span class="text-tiny">Created by Chris</span>
 	</section>
 </header>
 
 <style>
+	#created-by {
+		display: flex;
+	}	
+	
+	@media screen and (max-width: 400px) {
+		#created-by {
+			display: none;
+		}
+	}
+
 	figure {
 		display: flex;
 		overflow: hidden;
@@ -45,10 +57,10 @@
 	header {
 		padding: 1em;
 		background-color: #222831;
-		color: #EEEEEE;
+		color: #eeeeee;
 	}
 
-	[aria-current="page"] {
+	[aria-current='page'] {
 		font-weight: bold;
 	}
 </style>
